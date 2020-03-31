db530fba0ab5ab9fd955241adf83e2c29e671911
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 05:05:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,14 @@
 # code your #valid_move? method here
-def valid_move?
+def valid_move?(board, index)
+  if index >= 0 && index <= 8
+    if position_taken?(board,index) == false
+      true
+    else
+      false
+    end
+  else
+    false
+  end
 
 end
 
